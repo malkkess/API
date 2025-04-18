4d06b62935ce41a40936954d24055739ff9a1c58
hp <hp@DESKTOP-IN6GE2O>
2025-04-18 02:41:00 +0200

    DbContext

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -1,4 +1,5 @@
-
+using Microsoft.EntityFrameworkCore;
+using Presis.Data;
 namespace E_Commerce.Web
 {
     public class Program
@@ -13,6 +14,10 @@ namespace E_Commerce.Web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<StoreDbContext>(opt =>
+            {
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("DefualtConnection"));
+            });
 
             var app = builder.Build();
 
---
+++ Presis/Data/Config/ProductConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Presis.Data.Config
+{
+    public class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(p=>p.productBrand).WithMany().HasForeignKey(p=>p.BrandId);
+            builder.HasOne(p => p.productType).WithMany().HasForeignKey(p => p.TypeId);
+            builder.Property(p => p.Price).HasColumnType("decimal(10,2)");
+        }
+    }
+}
