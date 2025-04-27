1fb2ec6182b198c1b98ce04d8cf132dd76e50812
hp <hp@DESKTOP-IN6GE2O>
2025-04-27 12:47:35 +0300

    ExceptionMiddleWare

---
+++ E-Commerce.Web/CustomMiddleWares/CustomExcepMiddleWare.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using Shared.ErrorModels;
+
+namespace E_Commerce.Web.CustomMiddleWares
+{
+    public class CustomExcepMiddleWare
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<CustomExcepMiddleWare> logger;
+
+        public CustomExcepMiddleWare(RequestDelegate Next , ILogger<CustomExcepMiddleWare>logger)
+        {
+            _next = Next;
+            this.logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next.Invoke(httpContext);
+            }
+            catch(Exception ex)
+            {
+                logger.LogError(ex,"Something Went Wrong");
+                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                httpContext.Response.ContentType = "application/json";
+
+                var Response = new ErrorToReturn()
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError,
+                    ErrorMessage = ex.Message
+                };
+                
+                await httpContext.Response.WriteAsJsonAsync(Response);
+
+            }
+
+        }
+    }
+}
