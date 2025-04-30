49274d546721a4bf62c0ee21f089d7a61dda1bf5
hp <hp@DESKTOP-IN6GE2O>
2025-04-30 16:05:41 +0300

    FinalSession04

---
+++ Core/DomainLayer/Contracts/IBasketRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer.Models.BasketModels;
+
+namespace DomainLayer.Contracts
+{
+    public interface IBasketRepo
+    {
+        Task<CustomerBasket?> GetBasketAsync(String Key);
+        Task<CustomerBasket?> CreateOrUpdateBasketAsync(CustomerBasket basket, TimeSpan? TimeToLive = null);
+        Task<bool> DeleteBasketAsync(string id);
+    }
+}
---
+++ Infastructure/Presentation/Controllers/BasketController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceAbsrt;
+using Shared.DataTransfetObject.BasketMouldeDtos;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BasketController(IServiceManger serviceManger):ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<BasketDtos>>GetBasket(string Key)
+        {
+            var Basket =await serviceManger.BasketService.GetbasketAsync(Key);
+            return Ok(Basket);
+        }
+        [HttpPost]
+        public async Task<ActionResult<BasketDtos>>CretaeOrUpdaetBasket(BasketDtos basket)
+        {
+            var Basket = await serviceManger.BasketService.CreateOrUpdateBasketAsync(basket);
+            return Ok(Basket);
+        }
+        [HttpDelete("{Key}")]
+        public async Task <ActionResult<bool>>DeleteBasket(string Key)
+        {
+            var Result= await serviceManger.BasketService.DeletBasketAsync(Key);
+            return Ok(Result);
+        }
+    }
+}
